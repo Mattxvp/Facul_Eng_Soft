e9ed685ee2d4217033177c341e6f0e14d4a60582
Mattxvp <mathiasprolla@gmail.com>
2021-05-28 02:03:26 -0300

    AtualizacaoFinal1

--- Atv1Alg2.c
+++ Atv1Alg2.c
@@ -1,23 +1,29 @@
+//ATIVIDADE 1 - ALGORITIMOS E LÓGICA DE PROGRAMAÇÃO 1
+//Desenvolvido por: Mathias Prolla - 2021
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
+
 //_______________________________________________________________________
-int teste;
+
 int vJoao = 0, vMaria = 0, vZe = 0, vVotoNulo = 0, vInvalido = 0;
 int pZureta = 0, pGomes = 0, pVotoNulo = 0, pInvalido = 0;
 
 int main() {
 
     setlocale(LC_ALL,"Portuguese");
+
     int opcao_menu = 0, opcao_vereador = 0, opcao_prefeito = 0;
     system("cls");
 
-    printf("---------- MENU -----------\n");
-    printf(" 1 - VOTO \n");
-    printf(" 2 - APURAR OS VOTOS \n");
+    printf("||||||||| MENU PRINCIPAL ||||||||||||\n");
+    printf("\n");
+    printf(" 1 - VOTAR \n");
+    printf(" 2 - APURAÇÃO DOS VOTOS \n");
     printf(" 3 - SAIR \n");
-    printf("-------------------------\n");
-    printf("-->");  
+    printf("-------------------------------------\n");
+    printf("DIGITE O QUE VOCÊ DESEJA --> ");  
     scanf("%i", &opcao_menu);
     fflush(stdin);
 
@@ -25,13 +31,15 @@ int main() {
     {
     case 1:
         system("cls");
-        printf("--------- VOTAÇÃO-VEREADOR ------------\n");
+        printf("||||||||| VOTAÇÃO-VEREADOR |||||||||\n");
+        printf("\n");
         printf("111 - Vereador Joao do Frete\n");
         printf("222 - Vereador Maria da Pamonha\n");
         printf("333 - Vereador Ze da Farmacia\n");
         printf("444 - Voto nulo\n");
         printf("-----------------------------");
-        printf("--> ");
+        printf("\n");
+        printf("QUAL O SEU VOTO PARA VEREADOR? --> ");
         scanf("%i", &opcao_vereador);
         fflush(stdin);
         switch(opcao_vereador)
@@ -58,15 +66,17 @@ int main() {
             vInvalido = vInvalido + 1;
             break;
         }
-        printf("Pressione Enter para continuar ... ");
+        
         system("pause");
         system("cls");
-        printf("--------- VOTAÇÃO-PREFEITO ----------\n");
+        printf("||||||||| VOTAÇÃO-PREFEITO |||||||||\n");
+        printf("\n");
         printf("11 - Para Pr. Dr. Zureta\n");
         printf("22 - Para Pr. Senhor Gomes\n");
         printf("44 - Voto nulo\n");
         printf("-----------------------------");
-        printf("--> ");
+        printf("\n");
+        printf("QUAL O SEU VOTO PARA PREFEITO? --> ");
         scanf("%i", &opcao_prefeito);
         fflush(stdin);
         switch(opcao_prefeito)
@@ -85,33 +95,35 @@ int main() {
                 pVotoNulo = pVotoNulo + 1;
                 break;
             default:
-                printf("Voto confirmado em Prefeito Dr. Zureta!\n");
+                printf("Voto inválido!\n");
                 pInvalido = pInvalido + 1;
                 printf("Opção inválida!");
                 break;
             
         }
-        printf("Pressione Enter para continuar ... ");
+        
         system("pause");
         main();
         break;
 
     case 2:
         system("cls");
-        printf("--------- APURAÇÃO DOS VOTOS ----------\n");
+        printf("||||||||| APURAÇÃO DOS VOTOS ||||||||||\n");
+        printf("\n");
         printf("--------- VEREADORES ----------\n");
-        printf("Vereador 111: %d votos.\n", vJoao);
-        printf("Vereador 222: %d votos.\n", vMaria);
-        printf("Vereador 333: %d votos.\n", vZe);
-        printf("Vereador 444: %d votos.\n", vVotoNulo);
+        printf("Vereador Joao do Frete 111: %d votos.\n", vJoao);
+        printf("Vereador Maria da Pamonha 222: %d votos.\n", vMaria);
+        printf("Vereador Ze da Farmacia 333: %d votos.\n", vZe);
+        printf("Votos Nulos 444: %d votos.\n", vVotoNulo);
         printf("Votos Inválidos: %d votos.\n", vInvalido);
-        printf("--------- PREFEITOS ----------\n");
-        printf("Prefeito 11: %d votos.\n", pZureta);
-        printf("Prefeito 22: %d votos.\n", pGomes);
-        printf("Prefeito 44: %d votos.\n", pVotoNulo);
-        printf("Voto Inválido: %d votos.\n", pInvalido);
+        printf("\n");
+        printf("--------- PREFEITOS -----------\n");
+        printf("Prefeito Dr. Zureta 11: %d votos.\n", pZureta);
+        printf("Prefeito Senhor Gomes 22: %d votos.\n", pGomes);
+        printf("Votos Nulos 44: %d votos.\n", pVotoNulo);
+        printf("Votos Inválidos: %d votos.\n", pInvalido);
         printf("---------------------------------------\n");
-        printf("Pressione Enter para continuar ... ");
+    
         system("pause");
         main();
         break;
